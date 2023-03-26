6c1c97b89f9a8946223fef9b1810783025bba032
FedeDiaz31 <fede.diaz1231@gmail.com>
2023-03-26 18:19:40 -0300

    cart component actualizado

--- src/components/CartComponent.jsx
+++ src/components/CartComponent.jsx
@@ -1,30 +1,46 @@
 import { dividerClasses } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { removeProduct } from "../redux/cartReducer";
 
-function cartComponent({ setShowCart }) {
+function CartComponent({ setShowCart }) {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+
   const handleCloseCart = () => {
     setShowCart(false);
   };
 
   return (
     <>
-      <div className="w-[600px] h-[200px] bg-bgPrimaryColor border-solid rounded ">
-        <div className="flex ">
-          <img src="" alt="product-pic" className="mt-10 ml-5" />
-          <h2 className="text-bgSecondaryColor mt-10 absolute left-[150px]">
-            Título del carrito
-          </h2>
-        </div>
-        <div>
+      <div className="w-[600px]  bg-bgPrimaryColor border-solid rounded mt-10 mb-10 px-5">
+        {cart.map((product) => (
+          <div className="flex justify-between items-center">
+            <div className="flex items-center">
+              <img
+                src={product.image[0]}
+                alt="product-pic"
+                className="w-10 z-0  ml-5"
+              />
+              <h2 className="text-bgSecondaryColor ml-10">{product.model}</h2>
+            </div>
+            <div>
+              <DeleteIcon onClick={() => dispatch(removeProduct(product))} />
+            </div>
+          </div>
+        ))}
+
+        <div className="mt-5">
           <Link to={"/cart"}>
-            <button className="bg-bgPrimaryColor w-[540px] mx-[30px] absolute bottom-[70px] border-solid border-black rounded  ">
+            <button className="bg-bgPrimaryColor w-full    border-solid border-black rounded -5 ">
               Review cart
             </button>
           </Link>
         </div>
         <div>
           <button
-            className="bg-bgTertiaryColor w-[540px] rounded absolute bottom-5 mx-[30px]"
+            className="bg-bgTertiaryColor w-full   rounded  my-3 "
             onClick={handleCloseCart}
           >
             Close
@@ -35,4 +51,4 @@ function cartComponent({ setShowCart }) {
   );
 }
 
-export default cartComponent;
+export default CartComponent;
--- src/redux/cartReducer.js
+++ src/redux/cartReducer.js
@@ -3,20 +3,17 @@ import { createSlice, current } from "@reduxjs/toolkit";
 const initialState = [];
 
 const cartSlice = createSlice({
-    name: "cart",
-    initialState,
-    reducers: {
-        addProduct(state, action) {
-            console.log(action.payload);
-            console.log(current(state));
-            return [...state, action.payload];
-
-        },
-        removeProduct(state, action) {
-
-        }
-
+  name: "cart",
+  initialState,
+  reducers: {
+    addProduct(state, action) {
+      console.log(action.payload);
+      return [...state, action.payload];
     },
+    removeProduct(state, action) {
+      return state.filter((product) => product._id !== action.payload._id);
+    },
+  },
 });
 
 export const { addProduct, removeProduct } = cartSlice.actions;
