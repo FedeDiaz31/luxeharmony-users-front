2b089d81e501e86e8879f74dadfcbd1a8b1181a8
Alan Torino <alantorino42@gmail.com>
2023-03-27 16:20:51 -0300

    about link navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Nav } from "rsuite";
+import { Link } from "react-router-dom";
 
 const NavBar = () => (
   <Nav className="w-11/12 mx-auto mt-auto h-1/2 relative mx-auto items-start m-2 flex justify-around columns-4">
@@ -30,20 +31,10 @@ const NavBar = () => (
         <Nav.Item>Item E-4-3</Nav.Item>
       </Nav.Menu>
     </Nav.Menu>
-    <Nav.Menu
-      title="ABOUT US"
-      className="relative w-full bg-headerAndFooterColor px-2 pb-1"
-    >
-      <Nav.Item>Item E-1</Nav.Item>
-      <Nav.Item>Item E-2</Nav.Item>
-      <Nav.Item>Item E-3</Nav.Item>
-      <Nav.Item>Item E-4</Nav.Item>
-      <Nav.Menu title="Item E-4">
-        <Nav.Item>Item E-4-1</Nav.Item>
-        <Nav.Item active>Item E-4-2</Nav.Item>
-        <Nav.Item>Item E-4-3</Nav.Item>
-      </Nav.Menu>
-    </Nav.Menu>
+    <Nav.Item className="relative w-full bg-headerAndFooterColor px-2 pb-1">
+      <Link to="/about">ABOUT US</Link>
+    </Nav.Item>
+
     <Nav.Menu
       title="LOGIN"
       className="relative w-full bg-headerAndFooterColor px-2 pb-1"
