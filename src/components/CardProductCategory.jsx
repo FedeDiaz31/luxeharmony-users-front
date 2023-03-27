49cf92f5cacef536004356b106125895f5e305f6
magguer <martintelaguer@gmail.com>
2023-03-27 15:57:25 -0300

    logo-categories-product

--- src/components/CardProductCategory.jsx
+++ src/components/CardProductCategory.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
+function CardProductCategory({ slug, model, image, brand, price }) {
+  console.log(brand);
 
-function CardProductCategory({slug,model,image,brand,price}) {
-    return (
-        <Link
-          to={`/product/${slug}`}
-          className="cardProduct w-[80%]  flex flex-col items-center justify-center bg-bgPrimaryColor "
-        >  
-          <img className="w-[30vw] h-[60vh] border border-bgFourthColor object-contain" src={image} alt={brand + model} />
-          <div className='w-[100%] text-left'>
-            <img src={brand.logo} alt="" />
-            <h1 className='title-card-category'>{model}</h1>
-            <span>${price}</span>
-          </div>
-        </Link>
-      );
+  return (
+    <Link
+      to={`/product/${slug}`}
+      className="cardProduct w-[80%]  flex flex-col items-center justify-center bg-bgPrimaryColor "
+    >
+      <img
+        className="w-[30vw] h-[60vh] border border-bgFourthColor object-contain"
+        src={image}
+        alt={brand + model}
+      />
+      <div className="w-[100%] text-left">
+        <img
+          src={
+            brand.logo2.includes("http")
+              ? brand.logo2
+              : `${process.env.REACT_APP_API_URL}/img/${brand.logo2}`
+          }
+          alt=""
+        />
+        <h1 className="title-card-category">{model}</h1>
+        <span>${price}</span>
+      </div>
+    </Link>
+  );
 }
 
-export default CardProductCategory
+export default CardProductCategory;
--- src/routes/Category.jsx
+++ src/routes/Category.jsx
@@ -1,15 +1,13 @@
-import React, { useState,useEffect } from 'react'
-import FilterCategory from '../components/FilterCategory'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
-import CardProductCategory from '../components/CardProductCategory'
-
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import CardProductCategory from "../components/CardProductCategory";
 
 function Category() {
-const name = useParams().slug;
-const [products,setProducts]=useState(null)
+  const name = useParams().slug;
+  const [products, setProducts] = useState(null);
 
-useEffect(() => {
+  useEffect(() => {
     const getProducts = async () => {
       const response = await axios({
         method: "get",
@@ -20,14 +18,26 @@ useEffect(() => {
     getProducts();
   }, []);
 
-return (
-    <div className='w-[100vw]'>
-        <div className='w-100vw h-[40vh] bg-bgSecondaryColor'></div>
-         <div className=' w-[60vw] m-auto grid grid-cols-3 gap-3'>
-           { products ? products.map(product => <CardProductCategory slug={product.slug} model={product.model} brand={product.brand.logo} image={product.image[0]} price={product.price} />):<h1>cargando...</h1>}
-         </div>
+  return (
+    <div className="w-[100vw]">
+      <div className="w-100vw h-[40vh] bg-bgSecondaryColor"></div>
+      <div className=" w-[60vw] m-auto grid grid-cols-3 gap-3">
+        {products ? (
+          products.map((product) => (
+            <CardProductCategory
+              slug={product.slug}
+              model={product.model}
+              brand={product.brand}
+              image={product.image[0]}
+              price={product.price}
+            />
+          ))
+        ) : (
+          <h1>cargando...</h1>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
