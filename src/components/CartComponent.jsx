588f37a2c0647370fc4fde7c3867504dfc646262
magguer <martintelaguer@gmail.com>
2023-03-31 16:11:20 -0300

    cart-text-max-w

--- src/components/CartComponent.jsx
+++ src/components/CartComponent.jsx
@@ -48,7 +48,7 @@ function CartComponent({ setShowCart }) {
                   alt="product-pic"
                   className="w-10 z-0"
                 />
-                <h2 className="text-bgSecondaryColor hidden tablet:block">
+                <h2 className="text-bgSecondaryColor hidden tablet:block max-w-[160px] text-ellipsis overflow-hidden">
                   {product.model}
                 </h2>
               </div>
