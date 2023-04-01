e5ced5b9dab74547ec985fb0a83c00f5584fdc2c
Juan Manuel Ottado <juanmanuelottado@gmail.com>
2023-03-31 21:01:05 -0300

    fix card product

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -5,7 +5,7 @@ function CardProduct({ brand, image, model, slug }) {
   return (
     <Link
       to={`/product/${slug}`}
-      className="cardProduct w-[70vw] bg-bgTertiaryColor h-[90%] desktop:w-[20vw] desktop:h-[100%] flex flex-shrink-0 items-center justify-center"
+      className="cardProduct w-[70vw] h-[90%] desktop:w-[20vw] desktop:h-[100%] flex flex-shrink-0 items-center justify-center"
     >
       <h1>{model}</h1>
       <img
