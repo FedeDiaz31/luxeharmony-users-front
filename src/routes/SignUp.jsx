7ace1d94dedc56cc9cf7241c70e8db607d26df1b
magguer <martintelaguer@gmail.com>
2023-04-03 18:15:21 -0300

    stash-delete

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import axios from "axios";
-import slashImg from "../assets/img/slash.jpg";
 
 function SignUp() {
   const [firstname, setFirstname] = useState("");
